8c8d5efe47a902e644c123e61174b36afd3bad8d
winalex24-star <winalex24@gmail.com>
2026-09-22 14:05:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from pydantic import BaseModel
+import edge_tts
+import uuid
 
+app = FastAPI()
+
+class TTSRequest(BaseModel):
+    texto: str
+    voz: str = "es-US-AlonsoNeural"
+
+@app.post("/tts")
+async def tts(req: TTSRequest):
+
+    archivo = f"{uuid.uuid4()}.mp3"
+
+    communicate = edge_tts.Communicate(
+        req.texto,
+        req.voz
+    )
+
+    await communicate.save(archivo)
+
+    return FileResponse(
+        archivo,
+        media_type="audio/mpeg",
+        filename="audio.mp3"
+    )
